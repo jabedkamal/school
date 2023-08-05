3cf5fdcfc8a233631a52b9c71a7c77f85157a0ad
Jabed Kamal <63256395+jabedkamal@users.noreply.github.com>
2023-08-06 03:04:09 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example JavaScript code
+// You can add your own scripts and functionality here
+document.addEventListener("DOMContentLoaded", function () {
+    // Example code to interact with a button click
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", function () {
+        alert("Button clicked!");
+    });
+});
